05a8f29ee4ab27171a3fb2f687ba01e6fe87f7a6
tmbutcher <74381518+tmbutcher@users.noreply.github.com>
2025-08-25 14:16:24 -0500

    Demo_Dactyl functional
    Finally got it working

---
+++ config/key_positions.h
@@ -0,0 +1,70 @@
+/*  -------------------------------------------------- KEY POSITIONS------------------------------------------------------								
+																				    
+	╭────────────────────╮			╭────────────────────╮		╭─────────────────────╮			 ╭─────────────────────╮ 
+	│  0   1   2   3   4 │			│  5   6   7   8   9 │		│ LN1 LN2 LN3 LN4 LN5 │			 │ RN1 RN2 RN3 RN4 RN5 │
+	│ 10  11  12  13  14 |			│ 15  16  17  18  19 │		│ LT1 LT2 LT3 LT4 LT5 │			 │ RT1 RT2 RT3 RT4 RT5 │ 
+	| 20  21  22  23  24 |			| 26  27  28  29  30 |		| LM1 LM2 LM3 LM4 LM5 |			 | RM1 RM2 RM3 RM4 RM5 |
+	│ 31  32  33  34  35 ╰───╮	╭───╯ 36  37  38  39  40 │		│ LB1 LB2 LB3 LB4 LB5 ╰───╮	 ╭───╯ RB1 RB2 RB3 RB4 RB5 │ 
+	╰───────────╮ 41  42  43 │	│ 44  45  46 ╭───────────╯		╰───────────╮ LH1 LH2 LH3 │	 │ RH1 RH2 RH3 ╭───────────╯
+				╰───────╮ 47 │	│ 48 ╭───────╯ 								╰───────╮ LH4 │	 │ RH4 ╭───────╯ 
+						╰────╯	╰────╯												╰─────╯	 ╰─────╯				 */
+
+#pragma once
+
+#define LN1  0	// left number row
+#define LN2  1
+#define LN3  2
+#define LN4  3
+#define LN5  4
+
+#define RN1  5 // right number row
+#define RN2  6
+#define RN3  7
+#define RN4  8
+#define RN5  9
+
+#define LT1 10	// left top (letter) row
+#define LT2 11
+#define LT3 12
+#define LT4 13
+#define LT5 14
+
+#define RT1 15  // right top (letter) row
+#define RT2 16
+#define RT3 17
+#define RT4 18
+#define RT5 19
+
+#define LM1 20  // left-middle row
+#define LM2 21
+#define LM3 22
+#define LM4 23
+#define LM5 24
+
+#define RM1 25  // right-middle row
+#define RM2 26
+#define RM3 27
+#define RM4 28
+#define RM5 29
+
+#define LB1 30  // left-bottom row
+#define LB2 31
+#define LB3 32
+#define LB4 33
+#define LB5 34
+
+#define RB1 35  // right-bottom row
+#define RB2 36
+#define RB3 37
+#define RB4 38
+#define RB5 39
+
+#define LH1 41  // left thumb keys
+#define LH2 42
+#define LH3 43
+#define LH4 44
+
+#define RH1 45  // right thumb keys
+#define RH2 46
+#define RH3 47
+#define RH4 48
